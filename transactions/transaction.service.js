ff1df1b2a3a5d8aa393e244120c1151116fcc07d
Nnamdi <nnwabuokei@paga.com>
2020-11-14 23:20:16 +0100

    Implemented the fetchParentTransaction, fetchChildTransaction and the pagination method for pagination

---
+++ transactions/transaction.service.js
@@ -0,0 +1,80 @@
+const { default: axios } = require('axios');
+
+exports.fetchParentTransaction = async () => {
+   const response =  await axios.get('http://localhost:3000/parentRoute');
+   return response.data;
+   
+}
+
+exports.fetchChildTransaction = async () => {
+    const response = await axios.get('http://localhost:3000/childRoute');
+    return response.data;
+}
+
+exports.paginate = (
+    totalItems,
+    currentPage =1,
+    pageSize=2,
+    maxPages=2
+) => {
+
+//  Calculate the total pages
+let totalPages = Math.ceil(totalItems/pageSize);
+
+//Ensure the current page is not out of range 
+if (currentPage < 1) {
+    currentPage = 1;
+} else if (currentPage > totalPages) {
+    currentPage = totalPages;
+}
+
+let startPage, endPage;
+if (totalPages <= maxPages) {
+    //total pages is less than the maximum so show all pages
+
+    startPage = 1;
+    endPage=totalPages
+} else {
+    //total pages more than max so calculate start and end pages.
+    let maxPagesBeforeCurrentPage = Math.floor(maxPages/2);
+    let maxPagesAfterCurrentPage = Math.ceil(maxPages/2);
+    if (currentPage <= maxPagesBeforeCurrentPage) {
+        //current page near the start 
+        startPage = 1;
+        endPage = maxPages; 
+    } else if (currentPage + maxPagesAfterCurrentPage >= totalPages) {
+        //current page near the end 
+        startPage = totalPages - maxPages + 1;
+        endPage = totalPages;
+    }
+    else {
+        //current page somewhere in the middle
+        startPage = currentPage - maxPagesBeforeCurrentPage;
+        endPage = currentPage + maxPagesAfterCurrentPage;
+    }
+}
+
+//calculate start and end item indexed
+let startIndex = (currentPage - 1) * pageSize;
+let endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+
+//create an array of pages to repeat the page control 
+
+let pages = Array.from(Array((endPage + 1) - startPage).keys()).map(i => startPage + i);
+
+//return object with all pages properties required by the view
+return {
+    totalItems,
+    currentPage,
+    totalPages,
+    startPage,
+    endPage,
+    startIndex,
+    endIndex,
+    pages
+};
+
+}
+
+console.log(this.paginate(7,1));
+
