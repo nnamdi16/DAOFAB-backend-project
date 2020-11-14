a7e34ec191c001496284f14a45df75b7b2660294
Nnamdi <nnwabuokei@paga.com>
2020-11-14 23:16:34 +0100

    Implemented db server using json-server and created custom route

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const fs = require('fs');
 const jsonServer = require('json-server');
-let server = jsonServer.create();
-let router = jsonServer.router(require('./db')());
-let middlewares = jsonServer.defaults();
+const server = jsonServer.create();
+const router = jsonServer.router(require('./db')());
+const middlewares = jsonServer.defaults();
+const {fetchTransaction} = require('./transactions/transaction.controller')
+
 
 server.use(middlewares);
+
+server.get('/fetch',fetchTransaction);
+
 server.use(router);
 server.listen(3000, function () {
     console.log('JSON Server is running')
