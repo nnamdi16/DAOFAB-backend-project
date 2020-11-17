d0c6f78608cd76b3a93692eb703a5b0dfc09a3ee
Nnamdi <nnwabuokei@paga.com>
2020-11-17 04:54:31 +0100

    Added fetchParentTransaction controller and created an endpoint fetchParentTransaction

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router(require('./db')());
 const middlewares = jsonServer.defaults();
-const {fetchTransaction} = require('./transactions/transaction.controller')
+const {fetchTransaction, fetchParentTransaction} = require('./transactions/transaction.controller')
 
 
 server.use(middlewares);
 
 server.get('/fetch',fetchTransaction);
+server.get('/fetchParentTransaction' , fetchParentTransaction)
 
 server.use(router);
 server.listen(3000, function () {
